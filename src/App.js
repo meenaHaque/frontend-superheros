57050b0c6ee45db22aaf80cdaff0da839c8a9947
Meena Haque <meena.haque@simplybusiness.co.uk>
2020-10-23 16:37:54 +0100

    import superheroes file into app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React from 'react';
-
-const superHeros = [
-  {
-    id: 1,
-    name: "Wonderwoman",
-    description: "Can be invisible"
-  },
-  {
-    id: 2,
-    name: "Spiderman",
-    description: "Can climb walls"
-  }
-]
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {superHeros.map(hero => {
-//          return <h1>{hero.name}</h1>
-//       })}
-//     </div>
-//   );
-// }
+import Superheros from './Superheros'
 
 
 function App() {
   return (
     <div className="App">
       <ol>
-      {superHeros.map(hero => {
-         return <li>{hero.name}</li>
+      {Superheros.map(hero => {
+         return <li>{hero.name} - {hero.description}</li>
       })}
       </ol>
     </div>
