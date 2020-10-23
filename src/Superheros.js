9ef8397f7b9ff42894c8e6ace36b0ab0b99f0b99
Meena Haque <meena.haque@simplybusiness.co.uk>
2020-10-23 16:36:58 +0100

    add superheros file

---
+++ src/Superheros.js
@@ -0,0 +1,14 @@
+const superHeroes = [
+    {
+      id: 1,
+      name: "Wonderwoman",
+      description: "Superhuman strength"
+    },
+    {
+      id: 2,
+      name: "Spiderman",
+      description: "Can climb walls"
+    }
+  ]
+
+export default superHeroes
