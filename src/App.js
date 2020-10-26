e4feed0d3bd9e7eb97182c9c56f3d05445b00149
Meena Haque <meena.haque@simplybusiness.co.uk>
2020-10-26 10:36:03 +0000

    add inline styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <ol>
       {Superheros.map(hero => {
-         return <li>{hero.name} - {hero.description}</li>
+         return <li style={{color:"red", fontSize: "16px", padding: "10px 2px"}}>{hero.name} - {hero.description}</li>
       })}
       </ol>
     </div>
