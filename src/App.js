90c83348fd12c3e12b52dd6b91d32c80ae384d01
Meena Haque <meena.haque@simplybusiness.co.uk>
2020-10-30 16:28:39 +0000

    import superhero component in main app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import superheros from './superheros'
-
+import Superheros from './components/Superheros'
 
 function App() {
   return (
     <div className="App">
-      <ol>
-      {superheros.map(hero => {
-         return <li style={{color:"green", fontSize: "16px", padding: "10px 2px"}}>{hero.name} - {hero.description}</li>
-      })}
-      </ol>
+      <Superheros />
     </div>
   );
 }
