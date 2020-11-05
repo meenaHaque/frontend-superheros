b09656a465bb4d991e47c3b244bdd9a6ee63c0a3
Meena Haque <meena.haque@simplybusiness.co.uk>
2020-11-05 18:12:58 +0000

    add tests to component

---
+++ src/components/Superheros.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import  { render }from '@testing-library/react';
+import Superheros from '../components/Superheros';
+
+describe('Superheroes component', () => {
+  test('matches the superheroes snapshot', () => {
+    const component = renderer.create(<Superheros/>);
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Batwoman is in the list', () => {
+    const { getByText } = render(<Superheros />);
+    const batwoman = getByText(/batwoman/i);
+    expect(batwoman).toBeInTheDocument();
+  });
+});
+
+
+
+  
