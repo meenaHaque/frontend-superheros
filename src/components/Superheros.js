9f876e355363d1a0dfd20dc337c0f557f12580bc
Meena Haque <meena.haque@simplybusiness.co.uk>
2020-11-25 10:28:57 +0000

    fix path to superheros data

--- src/components/Superheros.js
+++ src/components/Superheros.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import superheros from '../superheros'
+import React from "react";
+import superheros from "../Superheros";
 
 const Superheros = () => {
-  const listStyles = {
-    color:"green", 
-    fontSize: "16px", 
-    padding: "10px 2px"
-  }
-  return (
-    <ol>
-      {superheros.map(hero => {
-        return <li style={listStyles}> {hero.name} - {hero.description}</li>
-      })}
-    </ol>
-   )
-}
+	const listStyles = {
+		color: "green",
+		fontSize: "16px",
+		padding: "10px 2px",
+	};
+	return (
+		<ol>
+			{superheros.map((hero) => {
+				return (
+					<li style={listStyles}>
+						{" "}
+						{hero.name} - {hero.description}
+					</li>
+				);
+			})}
+		</ol>
+	);
+};
 
-export default Superheros
+export default Superheros;
