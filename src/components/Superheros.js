04c65d115603e7fe520eed594ced378088973049
Meena Haque <meena.haque@simplybusiness.co.uk>
2020-12-02 11:18:48 +0000

    add backend to app

--- src/components/Superheros.js
+++ src/components/Superheros.js
@@ -1,18 +1,27 @@
-import React from "react";
-import superheros from "../Superheros";
+import React, { useEffect, useState } from "react";
+
+const listStyles = {
+	color: "green",
+	fontSize: "16px",
+	padding: "10px 2px",
+};
 
 const Superheros = () => {
-	const listStyles = {
-		color: "green",
-		fontSize: "16px",
-		padding: "10px 2px",
-	};
+	const [superheros, setSuperheros] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:3001/api/v1/superheros")
+			.then((response) => response.json())
+			.then((data) => {
+				setSuperheros(data);
+			});
+	});
+
 	return (
 		<ol>
 			{superheros.map((hero) => {
 				return (
-					<li style={listStyles}>
-						{" "}
+					<li key={hero.id} style={listStyles}>
 						{hero.name} - {hero.description}
 					</li>
 				);
